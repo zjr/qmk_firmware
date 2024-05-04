72ed66fd00dfb66e5c7bb6333aa36793a775636b
Zachary J. Rollyson <zach@adxsoftware.com>
2024-05-04 12:12:08 -0400

    fix: forgot I need to wrap with TD()

--- keyboards/keebio/iris/keymaps/zjr/keymap.c
+++ keyboards/keebio/iris/keymaps/zjr/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LOWER,   KC_ENT,                    SPC_CTL, RAISE,   TD_ALT_SWAP
+                                    KC_LGUI, LOWER,   KC_ENT,                    SPC_CTL, RAISE,   TD(TD_ALT_SWAP)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
