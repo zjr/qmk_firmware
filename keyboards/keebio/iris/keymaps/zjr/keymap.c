81a07487e196eda3cd3dd29ec1b7153079ca3e08
Zachary J. Rollyson <zach@adxsoftware.com>
2024-05-04 11:56:46 -0400

    feat: turn of td_alt tap, add space-control MT

--- keyboards/keebio/iris/keymaps/zjr/keymap.c
+++ keyboards/keebio/iris/keymaps/zjr/keymap.c
@@ -27,16 +27,17 @@ enum custom_keycodes {
 
 const int ESC_CTL = MT(MOD_LCTL, KC_ESC);
 const int DOT_ALT = MT(MOD_RALT, KC_PDOT);
+const int SPC_CTL = MT(MOD_LCTL, KC_SPC);
 const int ADJ_0 = LT(ADJUST, KC_0);
 
-enum {
-  TD_ALT_SWAP,
-};
+/* enum { */
+/*   TD_ALT_SWAP, */
+/* }; */
 
-tap_dance_action_t tap_dance_actions[] = {
-  // Tap once for left alt, twice for right alt
-  [TD_ALT_SWAP] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
-};
+/* tap_dance_action_t tap_dance_actions[] = { */
+/*   // Tap once for left alt, twice for right alt */
+/*   [TD_ALT_SWAP] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT), */
+/* }; */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_J,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     ESC_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_N,    KC_E,    KC_L,    KC_SCLN, KC_QUOT,
+     ESC_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,           KC_SPC,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   TD(TD_ALT_SWAP)
+                                    KC_LGUI, LOWER,   KC_ENT,                    SPC_CTL, RAISE,   DOT_ALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
